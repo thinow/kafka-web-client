22d8586e9e9d9dfae84b69aee9c96f5455be7ce7
Thierry Nowak <thinow@gmail.com>
2019-12-13 22:32:28 +0100

    host static files using Flask

--- application/web/server.py
+++ application/web/server.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, redirect
 
-web_server = Flask(__name__)
+web_server = Flask(
+    __name__,
+    static_folder='static',
+    static_url_path=''
+)
 
 
 @web_server.route('/')
+def main_page():
+    return redirect("index.html")
+
+
+@web_server.route('/api/hello')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World!'
