4e93c801203eb304ad71123671a29bd5e0377759
Thierry Nowak <thinow@gmail.com>
2019-12-17 21:26:53 +0100

    produce random people data

--- application/kafka/producer.py
+++ application/kafka/producer.py
@@ -1,8 +1,12 @@
 import json
+import random
+import uuid
 
+import names
 from confluent_kafka import Producer
 
 BROKERS = ('localhost:9092',)
+TOPIC = 'people'
 
 producer = Producer({'bootstrap.servers': ','.join(BROKERS)})
 
@@ -16,7 +20,9 @@ def delivery_report(error, msg):
         print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
 
 
-for index in range(100):
+print("Starting to produce...")
+
+for index in range(1000):
     # Trigger any available delivery report callbacks from previous produce() calls
     producer.poll(0)
 
@@ -24,10 +30,11 @@ for index in range(100):
     # will be triggered from poll() above, or flush() below, when the message has
     # been successfully delivered or failed permanently.
     data = {
-        'id': index,
-        'foo': 'bar'
+        'uuid': str(uuid.uuid4()),
+        'name': names.get_full_name(),
+        'age': random.randint(12, 99)
     }
-    producer.produce('mytopic', json.dumps(data).encode('utf-8'), callback=delivery_report)
+    producer.produce(TOPIC, json.dumps(data).encode('utf-8'), callback=delivery_report)
 
 # Wait for any outstanding messages to be delivered and delivery report
 # callbacks to be triggered.
