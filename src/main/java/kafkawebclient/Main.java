98dbca30baade13112a92f223fde069d07807596
Thierry Nowak <thinow@gmail.com>
2020-01-28 22:27:02 +0100

    spring boot application

--- src/main/java/kafkawebclient/Main.java
+++
@@ -1,9 +0,0 @@
-package kafkawebclient;
-
-import java.util.Arrays;
-
-class Main {
-    public static void main(String... args) {
-        System.out.println("Hello " + Arrays.toString(args));
-    }
-}
